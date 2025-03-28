283433821d42825043e6912442225b5946a70f92
hmrogers241 <hrogers@hlgsystems.com>
2025-03-28 11:19:19 -0500

    Updated Requirement for nesting

--- src/components/templates/Requirement.tsx
+++ src/components/templates/Requirement.tsx
@@ -9,8 +9,16 @@ function Requirement() {
           <DisclosureButton className="group py-2">
             <img className="group-data-[open]:size-7 size-5 justify-self-end"src="" alt="" />
           </DisclosureButton>
-          <DisclosurePanel className="col-span-3">
-            This message would be replaced with another disclosure
+          <DisclosurePanel className="grid grid-cols-3 col-span-3">
+            <Disclosure>
+              <p className="col-span-2 line-clamp-1">1. Nested Requirement</p>
+              <DisclosureButton className="group py-2">
+                <img className="group-data-[open]:size-6 size-4 justify-self-end"src="" alt="" />
+              </DisclosureButton>
+              <DisclosurePanel className="col-span-3">
+                This is a nested message
+              </DisclosurePanel>
+            </Disclosure>
           </DisclosurePanel>
         </Disclosure>
       </div>
