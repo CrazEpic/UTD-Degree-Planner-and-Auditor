b55365c4d7a1fef54902c7ca599bab978ddcf201
Daniel Nguyen <94246004+CrazEpic@users.noreply.github.com>
2025-04-04 14:29:59 -0500

    took some code for easy express route logging

--- src/server/index.ts
+++ src/server/index.ts
@@ -50,6 +50,42 @@ app.use(authorization)
 // routes
 app.use("/api", router)
 
+const replaceParams = (string) => {
+	let curr = string
+	let last = ""
+	let paramCount = 1
+	while (last !== curr) {
+		last = curr.slice()
+		// this is the pattern that express uses when you define your path param without a custom regex
+		curr = curr.replace("(?:([^\\/]+?))", `:param${paramCount++}`)
+	}
+	return curr
+}
+
+const fetchRoutes = (initialRouter) => {
+	const _fetchRoutes = (router, prefix = "") => {
+		const routes = []
+		router.stack.forEach(({ route, handle, name, ...rest }) => {
+			if (route) {
+				// routes registered directly on the app
+				const path = replaceParams(`${prefix}${route.path}`).replace(/\\/g, "")
+				routes.push({ path, methods: route.methods })
+			} else if (name === "router") {
+				// router middleware
+				const newPrefix = rest.regexp.source
+					.replace("\\/?(?=\\/|$)", "") // this is the pattern express puts at the end of a route path
+					.slice(1)
+					.replace("\\", "") // remove escaping to make paths more readable
+				routes.push(..._fetchRoutes(handle, prefix + newPrefix))
+			}
+		})
+		return routes
+	}
+	return _fetchRoutes(initialRouter)
+}
+
 app.listen(PORT, () => {
 	console.log(`Server is running on ${BASE_URL}`)
+	console.log("All available routes:")
+	console.log(fetchRoutes(app._router))
 })
