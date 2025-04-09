e046aca33be7ca9c453f098191371903a97418b3
Hayden Rogers <114021747+hrogers241@users.noreply.github.com>
2025-04-08 20:26:44 -0500

    Webdev (#6)
    * Correct tabs
    
    * API call buttons

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,17 @@
 import { UserCircleIcon } from "@heroicons/react/24/outline"
 
 function NavBar() {
-  return (
-    <>
-      <div className="flex flex-row justify-between items-center bg-[#154734] w-full h-[55px] border">
-        <div className="flex flex-row">
-          <p className="text-white hover:bg-red-300 py-3 px-4">Place</p>
-          <p className="text-white hover:bg-red-300 py-3 px-4">Holder</p>
-        </div>
-        <UserCircleIcon className="hover:bg-red-300 size-[32px] m-[15px]" color="#ffffff"></UserCircleIcon>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div className="flex flex-row justify-between items-center bg-[#154734] w-full h-[55px] border">
+                <div className="flex flex-row">
+                    <p className="text-white hover:bg-red-300 py-3 px-4">Place</p>
+                    <p className="text-white hover:bg-red-300 py-3 px-4">Holder</p>
+                </div>
+                <UserCircleIcon className="hover:bg-red-300 size-[32px] m-[15px]" color="#ffffff"></UserCircleIcon>
+            </div>
+        </>
+    )
 }
 
 export default NavBar
--- src/components/Small/CourseBlockView.tsx
+++ src/components/Small/CourseBlockView.tsx
@@ -1,10 +1,15 @@
 import { PlusIcon } from "@heroicons/react/24/solid"
 import { CourseBlock } from "../../types/degreeTest"
+import { Button } from "@headlessui/react"
+
+function click(message: string) {
+    console.log(message)
+}
 
 function CourseBlockView({course, name}: {course: CourseBlock, name: string}) {
 
-    {/* To be replaced with actual logic*/}
-    const planned = Math.random() > 0.5
+    {/* Replace with check in planned degree */}
+    const planned = false
 
     return (
         <div className="flex flex-row gap-[8px] border rounded-[10px] items-center justify-center p-[8px] pr-0 border-r-0 rounded-r-none">
@@ -17,9 +22,11 @@ function CourseBlockView({course, name}: {course: CourseBlock, name: string}) {
                     {name}
                 </p>
                 {planned ? (
-                    <PlusIcon className="size-[24px]  ml-auto mr-[15px]"></PlusIcon>
-                ) : (
                     <p className="underline ml-auto mr-[15px]">Planned</p>
+                ) : (
+                    <Button className="hover:bg-blue-200 size-[24px] ml-auto mr-[15px]" onClick={() => click("Add Course")}>
+                        <PlusIcon></PlusIcon>
+                    </Button>
                 )}
             </>
             )}
