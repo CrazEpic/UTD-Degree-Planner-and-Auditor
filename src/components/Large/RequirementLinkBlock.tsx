93c56bc4ff7804c536797e8a804e111f33bfeff3
Hayden Rogers <hayden.rogers@utdallas.edu>
2025-04-11 13:16:37 -0500

    Small changes

--- src/components/Large/RequirementLinkBlock.tsx
+++ src/components/Large/RequirementLinkBlock.tsx
@@ -1,8 +1,5 @@
-import { Button, Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/react"
 import { ChevronDownIcon, MinusIcon, PlusIcon } from "@heroicons/react/24/solid"
-import { Block, CourseBlock, FlagToggleBlock, MatcherGroupBlock, TextBlock } from "../../types/degreeTest"
 import ProgressBar from "../Small/Requirements/ProgressBar"
-import CourseBlockView from "../Small/CourseBlockView"
 
 function RequirementLinkBlock({name, progress}: {name: string, progress: number[]}) {
 
@@ -14,12 +11,12 @@ function RequirementLinkBlock({name, progress}: {name: string, progress: number[
                     <p className="line-clamp-1 justify-self-start">{name}</p>
                     <div className="flex flex-row ml-auto justify-self-end gap-2">
                         <ProgressBar progress={progress}></ProgressBar>
-                        <div className="flex flex-col gap-1">
-                            <button className="size-4 border rounded-sm">
-                                <PlusIcon></PlusIcon>
-                            </button>
+                        <div className="flex flex-row items-center gap-1">
                             <button className="size-4 border rounded-sm">
                                 <MinusIcon></MinusIcon>
+                            </button>
+                            <button className="size-4 border rounded-sm">
+                                <PlusIcon></PlusIcon>
                             </button>
                         </div>
                     </div>
